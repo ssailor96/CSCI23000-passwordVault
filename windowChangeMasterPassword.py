a300f83175c555a8e13f2c2c354b72a458a723cd
Sarah White <whitesag@umail.iu.edu>
2017-04-22 15:42:54 -0400

    Completed basic UI windows.

--- windowChangeMasterPassword.py
+++ windowChangeMasterPassword.py
@@ -1,1 +1,37 @@
 #change master password ui window
+
+from tkinter import *
+
+class WindowChangeMasterPassword(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+
+        self.wm_title("Change Master Password")
+
+        Label(self, text = "Change Master Password").grid(row = 0, columnspan = 2)
+
+        Label(self, text = "Current Password").grid(row = 1, column = 0) 
+        self.currentPassword = Entry(self)
+        self.currentPassword.grid(row = 1, column = 1)
+
+        Label(self, text = "New Password").grid(row = 2, column = 0) 
+        self.newPassword = Entry(self)
+        self.newPassword.grid(row = 2, column = 1)
+
+        self.btnSaveChanges = Button(self, text = "Save Changes", command = self.PlaceHolder)
+        self.btnSaveChanges.grid(row = 3, column = 0)
+
+        self.btnCancel = Button(self, text = "Cancel", command = self.PlaceHolder)
+        self.btnCancel.grid(row = 3, column = 1)
+
+        self.mainloop()
+
+    def PlaceHolder(self):
+
+        return
+
+def main():
+    wdoChangeMasterPassword = WindowChangeMasterPassword()
+
+if __name__ == "__main__":
+    main()
--- windowEditAccount.py
+++ windowEditAccount.py
@@ -1,1 +1,43 @@
 #edit account ui window
+
+from tkinter import * 
+
+class WindowEditAccount(Tk):
+    def __init__(self):
+        Tk.__init__(self)
+
+        self.wm_title("Edit Account")
+
+        Label(self, text = "Account Name").grid(row = 0, column = 0) #create a label called "Master Password" and places at 0,0
+        self.accountName = Entry(self)
+        self.accountName.grid(row = 0, column = 1)
+
+        Label(self, text = "Username").grid(row = 1, column = 0) #create a label called "Master Password" and places at 0,0
+        self.username = Entry(self)
+        self.username.grid(row = 1, column = 1)
+
+        Label(self, text = "Password").grid(row = 2, column = 0) #create a label called "Master Password" and places at 0,0
+        self.password = Entry(self)
+        self.password.grid(row = 2, column = 1)
+
+        Label(self, text = "URL").grid(row = 3, column = 0) #create a label called "Master Password" and places at 0,0
+        self.url = Entry(self)
+        self.url.grid(row = 3, column = 1)
+
+        self.btnSave = Button(self, text = "Save", command = self.PlaceHolder)
+        self.btnSave.grid(row = 4, column = 0)
+
+        self.btnCancel = Button(self, text = "Cancel", command = self.PlaceHolder)
+        self.btnCancel.grid(row = 4, column = 1)
+
+        self.mainloop()
+
+    def PlaceHolder(self):
+
+        return
+
+def main():
+    wdoEditAccount = WindowEditAccount()
+
+if __name__ == "__main__":
+    main()
