13d370683eb88d0790f2fadadf8cb5bd3714d522
Sarah White <whitesag@umail.iu.edu>
2017-04-23 16:28:37 -0400

    Working on generating passwords.

--- windowGeneratePassword.py
+++ windowGeneratePassword.py
@@ -13,21 +13,92 @@ lowerCase = "abcdefghijklmnopqrstuvwxyx"
 
 numbers = "0123456789"
 
+class Password(object):
+    def __init__(self, length, useSpecialChars, useUpperCase, useLowerCase, useNumbers):
+        object.__init__(self)
+        self.setLength(length)
+        self.setSpecialChars(bool(useSpecialChars))
+        self.setUpperCase(bool(useUpperCase))
+        self.setLowerCase(bool(useLowerCase))
+        self.setNumbers(bool(useNumbers))
+        
+    def getLength(self):
+        return self.length
+
+    def getSpecialChars(self):
+        return self.useSpecialChars
+
+    def getUpperCase(self):
+        return self.useUpperCase
+
+    def getLowerCase(self):
+        return self.useLowerCase
+
+    def getNumbers(self):
+        return self.useNumbers
+
+    def setLength(self, length):
+        self.length = length
+
+    def setSpecialChars(self, useSpecialChars):
+        self.useSpecialChars = useSpecialChars
+
+    def setUpperCase(self, useUpperCase):
+        self.useUpperCase = useUpperCase
+
+    def setLowerCase(self, useLowerCase):
+        self.useLowerCase = useLowerCase
+
+    def setNumbers(self, useNumbers):
+        self.useNumbers = useNumbers
+
+    def createPassword(self):
+        plainText = ""
+        #find checked checkboxes
+        #if boxes checked, take strings for those checkboxes and concatinate them
+        #scramble string
+        #use random to randomly select characters from string, getting the number of characters in length
+        #concatinate characters into password
+        #print in UI
+
+        if self.getSpecialChars() == True:
+            plainText += specialChars
+
+        if self.getUpperCase() == True:
+            plainText += upperCase
+
+        if self.getLowerCase() == True:
+            plainText += lowerCase
+
+        if self.getNumbers() == True:
+            plainText += numbers
+
+        lst = list(plainText)
+
+        random.shuffle(lst)
+
+        generatedKey = ''.join(lst)
+
+        print (generatedKey)
+
+        return
+
+
 class WindowGeneratePassword(Tk):
     def __init__(self):
         Tk.__init__(self)
 
-        self.wm_title("Generate Password") #creates title for window - "Change Maker"
+        self.wm_title("Generate Password") #creates title for window - " "
 
         Label(self, text = "Generate Password").grid(row = 0, columnspan = 2) #creates a label for Change Needed and places at 5,2
         
         Label(self, text = "Password").grid(row = 1, column = 0)
-        self.generatedPassword = Entry(self)
-        self.generatedPassword.grid(row = 1, column = 1)
+        self.txtGeneratedPassword = Entry(self)
+        self.txtGeneratedPassword.grid(row = 1, column = 1)
 
         Label(self, text = "Length").grid(row = 2, column = 0)
-        self.length = Entry(self)
-        self.length.grid(row = 2, column = 1)
+        self.txtLength = Entry(self)
+        self.txtLength.grid(row = 2, column = 1)
 
         self.cbSpecialCharsVal = IntVar()
         self.cbSpecialChars = Checkbutton(self, text = "Special Characters", variable = self.cbSpecialCharsVal)
@@ -45,7 +116,7 @@ class WindowGeneratePassword(Tk):
         self.cbNumbers = Checkbutton(self, text = "Numbers", variable = self.cbNumbersVal)
         self.cbNumbers.grid(row = 3, column = 2)
 
-        self.btnGenerate = Button(self, text = "Generate", command = self.PlaceHolder)
+        self.btnGenerate = Button(self, text = "Generate", command = self.CreateNewPassword)
         self.btnGenerate.grid(row = 4, column = 0)
 
         self.btnClose = Button(self, text = "Close", command = self.PlaceHolder)
@@ -55,6 +126,17 @@ class WindowGeneratePassword(Tk):
 
     def PlaceHolder(self):
         
+        return
+    
+    def CreateNewPassword(self):
+        length = int(self.txtLength.get())
+        useSpecialChars = self.cbSpecialCharsVal.get()
+        useUpperCase = self.cbUpperCaseVal.get()
+        useLowerCase = self.cbLowerCaseVal.get()
+        useNumbers = self.cbNumbersVal.get()
+        password = Password(length,useSpecialChars,useUpperCase,useLowerCase,useNumbers)
+        password.createPassword()
+
         return
 
 
